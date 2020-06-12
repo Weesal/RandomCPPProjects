054e80cee6af5f4ea39fa2c51ecc96597ed88c6b
Weesal <66493109+Weesal@users.noreply.github.com>
2020-06-12 00:34:48 -0500

    Add files via upload
    This is a basic program (that can be improved) that finds the value of "i" after a user enters any integer value. Ex.) i^25 = i.

---
+++ FindTheValueOf_i.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+/* This program finds the value of i after applying 
+   any integer value to it's power.
+*/
+
+int main()
+{
+   // Prompt user to enter what power i is to
+   cout << "Please enter what power i is to: ";
+   int powerOfI;
+   cin >> powerOfI;
+   
+   // Calculate what i would be
+   int powerDivisibleBy4, remainder;
+   string iToThe;
+   powerDivisibleBy4 = powerOfI / 4;
+   remainder = powerOfI - (4 * powerDivisibleBy4);
+   
+   // Determine i based on the left over power
+   if (remainder == 1)
+      iToThe = "i";
+   else if (remainder == 2)
+      iToThe = "-1";
+   else if (remainder == 3)
+      iToThe = "-i";
+   else if (remainder == 4)
+      iToThe = "1";
+   else if (remainder == 0)
+      iToThe = "1";
+      
+   // Display the result
+   cout << "i to the " << powerOfI << " is " << iToThe << endl;
+   
+   return 0;
+}
